8ab44a68e370c1fbb600756e32ddc3afd32ce08c
kashafdawood <kashaf.dawood@gmail.com>
2024-10-13 09:29:18 +0500

    setup mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, Logger } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-    })
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: async () => ({
+        uri: process.env.DATABASE_URI,
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const configService = app.get(ConfigService);
-  const globalPrefix = configService.get<string>('GLOBAL_PREFIX');
-  const port = configService.get<number>('PORT');
+  const globalPrefix = process.env.GLOBAL_PREFIX;
+  const port = process.env.PORT;
 
   app.setGlobalPrefix(globalPrefix);
   await app.listen(port);
