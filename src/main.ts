73286044f920de5fa20e019cad1c2a8a89b5ab6d
kashafdawood <kashaf.dawood@gmail.com>
2024-10-17 13:58:59 +0500

    configure cookie parser to access the httpOnly cookie at frontend

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import * as cookieParse from 'cookie-parser';
 
 dotenv.config();
 
@@ -8,6 +9,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = process.env.GLOBAL_PREFIX;
   const port = process.env.PORT;
+
+  app.use(cookieParse());
 
   app.setGlobalPrefix(globalPrefix);
   await app.listen(port);
