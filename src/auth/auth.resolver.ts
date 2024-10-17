d70610bd2a58ab898b4bc9d1d45fa4ecf8dbd949
kashafdawood <kashaf.dawood@gmail.com>
2024-10-17 13:56:31 +0500

    send access and refresh token to httpOnly cookie

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,17 +1,34 @@
-import { Mutation, Resolver, Args } from '@nestjs/graphql';
+import { Mutation, Resolver, Args, Context } from '@nestjs/graphql';
 import { AuthService } from './auth.service';
 import { User } from 'src/user/user.schema';
 import { SignUpInput } from './dto/signUp-user.input';
 import { SignInInput } from './dto/signIn-user.input';
 import { AuthResponse } from './dto/auth-response.dto';
+import { Response } from 'express';
 
 @Resolver((of) => User)
 export class AuthResolver {
   constructor(private authService: AuthService) {}
 
   @Mutation((returns) => AuthResponse)
-  signIn(@Args('signInInput') signInInput: SignInInput): Promise<any> {
-    return this.authService.signIn(signInInput);
+  async signIn(
+    @Args('signInInput') signInInput: SignInInput,
+    @Context('res') res: Response,
+  ): Promise<any> {
+    const { accessToken, refreshToken } =
+      await this.authService.signIn(signInInput);
+
+    res.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+    });
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+    });
+
+    return { accessToken, refreshToken };
   }
 
   @Mutation((returns) => User)
