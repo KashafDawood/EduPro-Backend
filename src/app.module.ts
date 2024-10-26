418b61e22b7d9a29797d501808f24e24f628893a
kashafdawood <kashaf.dawood@gmail.com>
2024-10-26 06:38:55 +0500

    create teacher module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from 'src/auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './auth/auth.guard';
 import { StudentModule } from './student/student.module';
+import { TeacherModule } from './teacher/teacher.module';
 
 @Module({
-  imports: [UserModule, AuthModule, ConfigrationModule, StudentModule],
+  imports: [UserModule, AuthModule, ConfigrationModule, StudentModule, TeacherModule],
   providers: [
     {
       provide: APP_GUARD,
