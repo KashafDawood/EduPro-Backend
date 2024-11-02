c2934a9bf1defc4f385d5f2526036994ba0dc95f
M Talha Masood <talha8665i@gmail.com>
2024-11-02 18:07:15 +0500

    aggregate example added

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -8,5 +8,25 @@ import { BaseService } from 'src/base.service';
 export class StudentService extends BaseService<Student> {
   constructor(@InjectModel(Student.name) private studentModel: Model<Student>) {
     super(studentModel);
+    // const data = studentModel.aggregate([
+    //   {
+    //     $match:
+    //       /**
+    //        * query: The query in MQL.
+    //        */
+    //       {
+    //         _id: ObjectId(id)
+    //       }
+    //   },
+    // {
+    //       $lookup: {
+    //         from: "subjects",
+    //         localField: "subajectId",
+    //         foreignField: "_id",
+    //         as: "subjects",
+    //       },
+        
+    // }
+    // ])
   }
 }
