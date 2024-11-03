d2245fbc9b1d696956a2f1f156bc0e20e0e6707c
kashafdawood <kashaf.dawood@gmail.com>
2024-11-03 14:46:22 +0500

    remove commit

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -10,26 +10,6 @@ import { Types } from 'mongoose';
 export class StudentService extends BaseService<Student> {
   constructor(@InjectModel(Student.name) private studentModel: Model<Student>) {
     super(studentModel);
-    // const data = studentModel.aggregate([
-    //   {
-    //     $match:
-    //       /**
-    //        * query: The query in MQL.
-    //        */
-    //       {
-    //         _id: ObjectId(id)
-    //       }
-    //   },
-    // {
-    //       $lookup: {
-    //         from: "subjects",
-    //         localField: "subajectId",
-    //         foreignField: "_id",
-    //         as: "subjects",
-    //       },
-
-    // }
-    // ])
   }
 
   async getStudentById(id: string): Promise<Student> {
